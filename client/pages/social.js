3561ede187b49667fdd7657fd0b705886fe829e8
Martin Aspeli <optilude@gmail.com>
2014-08-15 00:00:51 +0100

    More examples

--- client/pages/social.js
+++ client/pages/social.js
@@ -1,3 +1,10 @@
+Template.social.showPosts = function() {
+    // dumb example to test reactivity; do `Session.set('showPosts', false)` in
+    // the console to hide posts
+    var showPosts = Session.get('showPosts');
+    return showPosts !== undefined? showPosts : true;
+};
+
 Template.social.rendered = function() {
     var list = document.querySelector('post-list');
     var tabs = document.querySelector('paper-tabs');
